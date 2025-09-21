671e73fe2ba0c960763e2260ced25b5ac69b0ed5
Mayank-2k04 <mayankkapoor.work@gmail.com>
2025-09-21 15:07:19 +0530

    add auth and token generator

---
+++ auth.py
@@ -0,0 +1,16 @@
+from jose import jwt, JWTError
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from createtoken import SECRET_KEY, ALGORITHM
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        user_email = payload.get("email")
+        return {"user_id":user_id, "user_email":user_email}
+
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
